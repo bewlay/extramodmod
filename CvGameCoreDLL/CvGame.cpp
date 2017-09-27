486ccf489f6dcc196d6b4e2d8b4987a70fdc6fc8
lfgr <none@none>
2017-09-27 14:45:01 +0200

    Codechange: CvGame::getUnitCreatedCount is now const (this was likely an oversight)
    --HG--
    branch : events_enhanced

--- CvGameCoreDLL/CvGame.cpp
+++ CvGameCoreDLL/CvGame.cpp
@@ -5731,7 +5731,10 @@ void CvGame::setForceControl(ForceControlTypes eIndex, bool bEnabled)
 }
 
 
-int CvGame::getUnitCreatedCount(UnitTypes eIndex)
+// UNIVERSAL_PREREQS 09/2017 lfgr
+//int CvGame::getUnitCreatedCount(UnitTypes eIndex)
+int CvGame::getUnitCreatedCount(UnitTypes eIndex) const
+// UNIVERSAL_PREREQS end
 {
 	FAssertMsg(eIndex >= 0, "eIndex is expected to be non-negative (invalid Index)");
 	FAssertMsg(eIndex < GC.getNumUnitInfos(), "eIndex is expected to be within maximum bounds (invalid Index)");
