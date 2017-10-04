5e0dea182e3085ea264f9ac4858f39382ba2e001
lfgr <none@none>
2017-10-04 22:02:25 +0200

    Codechange: Added CvGameUnitNotCreatedPrereq
    --HG--
    branch : events_enhanced

---
+++ CvGameCoreDLL/CvUniversalPrereqsGame.h
@@ -0,0 +1,43 @@
+/*
+ * CvUniversalPrereqsGame.h
+ *
+ *  Created on: Sep 24, 2017
+ */
+
+#ifndef CVUNIVERSALPREREQSGAME_H_
+#define CVUNIVERSALPREREQSGAME_H_
+
+
+#include "CvEnums.h"
+
+#include "CvUniversalPrereqs.h"
+
+class CvGame;
+
+
+/**
+ * Requires that the specified unit wasn't ever created
+ */
+class CvGameUnitNotCreatedPrereq : public CvPrereq<CvGame>
+{
+public :
+	CvGameUnitNotCreatedPrereq( UnitTypes eUnit );
+
+	virtual ~CvGameUnitNotCreatedPrereq();
+
+	bool isValid( const CvGame* pGame ) const;
+
+
+	static const std::string TAG;
+
+	static CvGameUnitNotCreatedPrereq* read( CvXMLLoadUtility* pXml );
+
+private :
+	UnitTypes m_eUnit;
+};
+
+
+
+
+
+#endif /* CVUNIVERSALPREREQSGAME_H_ */
